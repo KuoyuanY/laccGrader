997e947966e91543c3688ee78629a479507d16d3
Dangoat <danevans@terpmail.umd.edu>
2017-11-04 00:50:31 -0400

    academic rubric schema

---
+++ lacc2/models/AcademicRubric.js
@@ -0,0 +1,46 @@
+var mongoose = require('mongoose');
+
+var academicSchema = new mongoose.Schema({
+  gpa: {
+    type: Number,
+    min: 0,
+    max: 3,
+    required: true
+  },
+  rank: {
+    type: Number,
+    min: 0,
+    max: 3,
+    required: true
+  },
+  apclasses: {
+    type: Number,
+    min: 0,
+    max: 3,
+    required: true
+  },
+  honorsclasses: {
+    type: Number,
+    min: 0,
+    max: 3,
+    required: true
+  },
+  apgrades: {
+    type: Number,
+    min: 0,
+    max: 3,
+    required: true
+  },
+  discretionary: {
+    type: Number,
+    min: 0,
+    max: 3,
+    required: true
+  }
+});
+
+var AcademicRubric = mongoose.model('AcademicRubric', academicSchema);
+
+module.exports = AcademicRubric
+
+
