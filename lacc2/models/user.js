1d205d7f2763bf7d6eacd86a8eecd42ec7eeb12b
Kuoyuan Yang <1kuoyuanyang@gmail.com>
2017-11-04 03:04:32 -0400

    user.js schema

---
+++ lacc2/models/user.js
@@ -0,0 +1,54 @@
+var mongoose = require('mongoose');
+var UserSchema = new mongoose.Schema({  
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    passwordConf: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: Number,
+        required: true
+    }    
+});
+UserSchema.statics.authenticate = function (username, password, callback) {
+  User.findOne({ username: username })
+    .exec(function (err, user) {
+      if (err) {
+        return callback(err)
+      } else if (!user) {
+        var err = new Error('User not found.');
+        err.status = 401;
+        return callback(err);
+      }
+      bcrypt.compare(password, user.password, function (err, result) {
+        if (result === true) {
+          return callback(null, user);
+        } else {
+          return callback();
+        }
+      })
+    });
+}
+//hashing a password before saving it to the database                                                                                                                                                       
+UserSchema.pre('save', function (next) {
+  var user = this;
+  bcrypt.hash(user.password, 10, function (err, hash){
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  })
+});
+
+var User = mongoose.model('User', UserSchema);
+module.exports = User;
